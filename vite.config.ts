ad091779ba0a0c3b03d5a0e29656ecf77b326d44
CianDonlan <83826786+CianDonlan@users.noreply.github.com>
2025-09-19 19:15:07 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: '/easymaths-ie/',
   server: {
     host: "::",
     port: 8080,
